980dab4e55a986b74be74b9023025388d84a013f
springsoft <40840171@qq.com>
2017-08-22 10:04:44 +0800

    Update views.py

--- sign/views.py
+++ sign/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Django!")
+    return HttpResponse("网站建设中，请关注!")
 
 def api(request):
     return JsonResponse({'status':100, 'message':'event name already exists'})
